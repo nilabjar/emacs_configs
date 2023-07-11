81a2fe2f0bafac4b165c8eb6c0e034b9e3a98fd2
nilabjar <nilabjar@gmail.com>
2023-07-10 22:36:03 -0400

    add counsel-etags

--- eglot/init.el
+++ eglot/init.el
@@ -95,6 +95,18 @@
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
 
+(use-package counsel-etags
+  :ensure t
+  :bind (("C-]" . counsel-etags-find-tag-at-point))
+  :init
+  (add-hook 'prog-mode-hook
+        (lambda ()
+          (add-hook 'after-save-hook
+            'counsel-etags-virtual-update-tags 'append 'local)))
+  :config
+  (setq counsel-etags-update-interval 60)
+  (push "build" counsel-etags-ignore-directories))
+
 ;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
