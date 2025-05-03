e43cd6799fbf8856d162623dc77f55e772916435
Nilabja Roy <NROY27@bloomberg.net>
2025-05-02 21:16:25 -0400

    Updating lsp-emacs

--- lsp-emacs/init.el
+++ lsp-emacs/init.el
@@ -1,47 +1,34 @@
-;; (require 'package)
+(require 'package)
 
 (setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
-(setq package-selected-packages
-      '(
-;;        lsp-mode
-;;        yasnippet
-;;        lsp-treemacs
-;;        helm-lsp
-;;        projectile
-;;        hydra
-;;        flycheck
-;;        avy
-;;        helm-xref
-;;        dap-mode
-;;        deadgrep
-;;        pyvenv
-;;        python-black
-	)
-)
-
-
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
-
-(setq use-package-compute-statistics t)
-
 (xterm-mouse-mode)
 (setq mouse-wheel-scroll-amount '(3))
-(setq use-package-always-defer t)
+;; (setq use-package-always-defer t)
 (load-theme 'wombat)
 
 (use-package hydra :ensure t)
 (use-package flycheck :ensure t)
 (use-package yasnippet :ensure t)
 
-(use-package projectile
+(use-package ripgrep :ensure t)
+
+(use-package projectile :ensure t)
+
+(use-package counsel-etags
   :ensure t
-  :commands (projectile-mode))
+  :bind (("C-]" . counsel-etags-find-tag-at-point))
+  :init
+  (add-hook 'prog-mode-hook
+        (lambda ()
+          (add-hook 'after-save-hook
+            'counsel-etags-virtual-update-tags 'append 'local)))
+  :config
+  (setq counsel-etags-update-interval 60)
+  (push "build" counsel-etags-ignore-directories))
 
 (use-package sr-speedbar
   :ensure t
@@ -57,67 +44,69 @@
 ;; 	  (lambda ()
 ;; 	    (load-theme 'wombat)))
 
-(use-package doom-themes
-  :defer t
+(use-package doom-themes :ensure t)
+
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+
+(use-package magit
+  :ensure t
+
   :config
-  :commands (load-theme))
+  (setq magit-disabled-section-inserters t)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-tags-header)
+  ;; (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent)
+  )
+
+
+(use-package deadgrep :ensure t)
+
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 (use-package evil
+  :ensure t
   :init
   (setq evil-disable-insert-state-bindings t)
   :config
-  (evil-mode)
   (evil-set-initial-state 'magit-popup-mode 'emacs)
   (evil-set-initial-state 'compilation-mode 'emacs)
   (evil-set-initial-state 'shell-mode 'emacs)
   (evil-set-initial-state 'eshell-mode 'emacs)
+  (evil-set-initial-state 'deadgrep-mode 'emacs)
   (evil-set-initial-state 'speedbar-mode 'emacs)
-  :hook (prog-mode text-mode)
-  :commands (evil-emacs-state))
+  (evil-set-initial-state 'apropos-mode 'emacs)
+  (evil-set-initial-state 'treemacs-mode 'emacs)
+  (evil-set-initial-state 'dired-mode 'emacs)
+  :hook (prog-mode text-mode))
 
 (use-package evil-commentary
   :ensure t
-  :config
-  (evil-commentary-mode)
   :hook (prog-mode text-mode))
 
 (use-package evil-snipe
   :ensure t
   :config
-  (evil-snipe-mode)
   (setq evil-snipe-scope 'whole-buffer)
   :hook (prog-mode text-mode))
 
-(use-package magit
+(use-package evil-surround
   :ensure t
-  :commands (magit-status))
-
-(use-package diff-hl
-  :ensure t
-  :init
   :config
-  (global-diff-hl-mode)
-  (diff-hl-dired-mode)
-  (diff-hl-margin-mode)
-  :commands (diff-hl-mode)
-  :hook (prog-mode text-mode))
+  (global-evil-surround-mode 1))
 
-(use-package deadgrep
-  :ensure t
-  :commands (deadgrep))
 
-(use-package which-key
-  :demand t
-  :config
-  (which-key-mode))
+;; (load-file "~/my_bigstore/alternate_home/emacs_configs/test_lsp/lsp_setup.el")
 
-(use-package company
-  :hook
-  (after-init . global-company-mode)
-  (shell-mode . (lambda() (company-mode 0)))
-  )
+;; (add-hook 'python-mode-hook 'eglot-ensure)
+;; (add-hook 'c++-mode-hook 'eglot-ensure)
 
 (use-package lsp-mode
+  :ensure t
   :config
   (setq lsp-keymap-prefix "C-c l")
   ;; Disable trying format while typing
@@ -127,15 +116,13 @@
          (c-mode . lsp)
          (c++-mode . lsp)
          ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
+         (lsp-mode . lsp-enable-which-key-integration)))
 
 ;; optionally
 ;; (use-package lsp-ui :ensure t :commands lsp-ui-mode)
 ;; if you are helm user
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+(use-package helm-lsp :ensure t)
 
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 (use-package lsp-pyright
   :ensure t
@@ -143,6 +130,37 @@
   :hook (python-mode . (lambda ()
                           (require 'lsp-pyright)
                           (lsp))))  ; or lsp-deferred
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(use-package diff-hl
+  :ensure t
+  :init
+  :config
+  (global-diff-hl-mode)
+  (diff-hl-dir-mode)
+  (diff-hl-margin-mode)
+  :hook (prog-mode text-mode))
+
+
+(use-package which-key
+  :ensure t
+  :demand t
+  :config
+  (which-key-mode))
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay nil)
+  :hook
+  (after-init . global-company-mode)
+  ;; (shell-mode . (lambda() (company-mode 0)))
+  )
+
 
 (use-package pyvenv
   :ensure t
@@ -153,13 +171,27 @@
   :ensure t
   :hook (python-mode . python-black-on-save-mode))
 
+(use-package dap-mode
+  :ensure t)
 
+(require 'dap-python)
+;; if you installed debugpy, you need to set this
+;; https://github.com/emacs-lsp/dap-mode/issues/306
+(setq dap-python-debugger 'debugpy)
+
+(add-hook 'dap-stopped-hook
+          (lambda (arg) (call-interactively #'dap-hydra)))
 ;; optionally if you want to use debugger
-(use-package dap-mode :commands dap-mode)
+;; (use-package dap-mode
+;;   :ensure t
+;;   :init
+;;   (setq dap-python-debugger 'debugpy)
+;;   :commands dap-mode)
 ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
 
 ;; optional if you want which-key integration
 (use-package which-key
+  :ensure t
   :config
   (which-key-mode))
 
@@ -171,25 +203,7 @@
 ;;   :commands (avy-goto-word-1)
 ;;   )
 
-(use-package helm
-  :defer t
-  :commands (helm-mini)
-  :bind (("M-x" . helm-M-x)
-         ("C-x b" . helm-mini)
-         ("C-x C-f" . helm-find-files)
-         ("C-x C-d" . helm-browse-project)
-         ("M-y" . helm-show-kill-ring))
-  :config  (helm-mode 1)
-  (setq helm-M-x-fuzzy-match 1)
-  (setq helm-buffers-fuzzy-matching 1)
-  (setq helm-recentf-fuzzy-match 1)
-  ;; Set buffer list column size to max length
-  (setq helm-buffer-max-length nil)
-  )
-
-(use-package helm-xref
-  :ensure t
-  :after (helm))
+(load-file "~/my_bigstore/alternate_home/emacs_configs/test_lsp/helm.el")
 
 (use-package git-link
   :ensure t
@@ -201,15 +215,6 @@
                   '("bbgithub\\.dev\\.bloomberg\\.com" git-link-commit-github)))
   )
 
-;; (eval-after-load 'git-link
-;;  '(progn
-;;    (add-to-list 'git-link-remote-alist
-;;      '("bbgithub" git-link-github))
-;;    (add-to-list 'git-link-commit-remote-alist
-;;      '("bbgithub\\.dev\\.bloomberg\\.com" git-link-commit-github))))
-
-
-
 ;; ;
 ;;                                         ; (helm-mode)
 ;; ;; (require 'helm-xref)
@@ -217,20 +222,6 @@
 ;; ;; (define-key global-map [remap execute-extended-command] #'helm-M-x)
 ;; ;; (define-key global-map [remap switch-to-buffer] #'helm-mini)
 
-(setq gc-cons-threshold (* 100 1024 1024)
-      company-idle-delay 0.0
-      company-minimum-prefix-length 1
-      lsp-idle-delay 0.1)  ;; clangd is fast
-
-;; (with-eval-after-load 'lsp-mode
-;;   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-;;   (require 'dap-cpptools)
-;;   (require 'dap-python)
-;;   (setq dap-auto-configure-features '(sessions locals controls tooltip))
-;;   ;; if you installed debugpy, you need to set this
-;;   ;; https://github.com/emacs-lsp/dap-mode/issues/306
-;;   (setq dap-python-debugger 'debugpy)
-;;   (yas-global-mode))
 
 ;; ====================================
 ;; Development Setup
@@ -281,132 +272,89 @@
   (general-define-key
    :states '(normal visual insert)
    :prefix "SPC"
-   :non-normal-prefix "C-f"
+   :non-normal-prefix "C-j"
 
    "p"  '(:ignore p :which-key "project")
    "pc" '(projectile-compile-project :which-key "project compile")
-   "pt" '(helm-etags-select :which-key "find tags")
+   "pt" '(counsel-etags-find-tag-at-point :which-key "find tags")
    "pf" '(projectile-find-file :which-key "find file in project")
+   "p/" '(deadgrep :which-key "Deadgrep")
 
    "f"  '(:ignore f :which-key "file")
-   "ft" '(list-tags :which-key "list tags in file")
+   "ft" '(counsel-etags-list-tag-in-current-file :which-key "list tags in file")
    "fe" '(sr-speedbar-toggle :which-key "speedbar-toggle")
    "fs" '(ff-get-other-file  :which-key "switch header/source")
    "fo" '(helm-find-files  :which-key "open file")
-   "fe" '(sr-speedbar-toggle :which-key "speedbar")
+   "fr" '(recentf-open :which-key "recent files")
 
    "b"  '(:ignore f :which-key "buffer")
    "bo" '(evil-buffer :which-key "other buffer")
    "bb" '(helm-mini :which-key "other buffer")
    "bd" '(kill-buffer :which-key "kill buffer")
    "bf" '(delete-other-windows :which-key "Fullscreen buffer")
+   "bc" '(company-complete :which-key "Invoke completion")
 
    "h" '(:ignore fh :which-key "hunks")
    "hn" '(diff-hl-next-hunk  :which-key "next hunk")
 
-   "*" '(projectile-grep  :which-key "project-wide grep")
+   "*" '(rg-menu  :which-key "project-wide grep")
 
    "g"  '(:ignore f :which-key "global")
    "gh" '(highlight-symbol-at-point :which-key "highlight point")
    "gu" '(unhighlight-regexp :which-key "unhighlight phrase")
+   "gf" '(highlight-regexp '"[a-z].*(" :which-key "Highlight Functions")
    ))
 
-(use-package dape
-  ;; :preface
-  ;; By default dape shares the same keybinding prefix as `gud'
-  ;; If you do not want to use any prefix, set it to nil.
-  ;; (setq dape-key-prefix "\C-x\C-a")
+;; ==========================================================================
+;;                Ripgrep
+;; ==========================================================================
 
-  ;; :hook
-  ;; Save breakpoints on quit
-  ;; (kill-emacs . dape-breakpoint-save)
-  ;; Load breakpoints on startup
-  ;; (after-init . dape-breakpoint-load)
+;; (grep-apply-setting
+;;    'grep-find-command
+;;    '("rg -n -H --no-heading -e '' $(git rev-parse --show-toplevel || pwd)" . 27)
+;;  )
 
-  ;; :config
-  ;; Turn on global bindings for setting breakpoints with mouse
-  ;; (dape-breakpoint-global-mode)
+;; (setq grep-command "rg -n -H --no-heading -e '' $(git rev-parse --show-toplevel || pwd)")
+;; (setq grep-command '("rg -n -H --no-heading -e '' $(git rev-parse --show-toplevel || pwd)" . 27))
 
-  ;; Info buffers to the right
-  ;; (setq dape-buffer-window-arrangement 'right)
+(setq grep-program "rg")
 
-  ;; Info buffers like gud (gdb-mi)
-  ;; (setq dape-buffer-window-arrangement 'gud)
-  ;; (setq dape-info-hide-mode-line nil)
+(use-package rg
+  :ensure t
+  :config
+  (rg-enable-default-bindings))
 
-  ;; Pulse source line (performance hit)
-  ;; (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+;; ==========================================================================
 
-  ;; Showing inlay hints
-  ;; (setq dape-inlay-hints t)
-
-  ;; Save buffers on startup, useful for interpreted languages
-  ;; (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
-
-  ;; Kill compile buffer on build success
-  ;; (add-hook 'dape-compile-hook 'kill-buffer)
-
-  ;; ;; Projectile users
-  ;; (setq dape-cwd-function 'projectile-project-root)
-  )
-
-;; Enable repeat mode for more ergonomic `dape' use
-;; (use-package repeat
-;;   :config
-;;   (repeat-mode))
-
-;;========== Space leader keys ========================
-;(use-package general
-;  :config
-;  (general-evil-setup t)
-;
-;  (general-create-definer rune/leader-keys
-;    :keymaps '(normal insert visual emacs)
-;    :prefix "SPC"
-;    :global-prefix "M-SPC"))
-;
-;(rune/leader-keys
-;  "t"  '(:ignore t :which-key "toggles")
-;  "tt" '(counsel-load-theme :which-key "choose theme")
-;
-;  "p"  '(:ignore p :which-key "project")
-;  "pc" '(projectile-compile-project :which-key "project compile")
-;  "pt" '(counsel-etags-find-tag-at-point :which-key "find tags")
-;  "pf" '(projectile-find-file :which-key "find file in project")
-;
-;  "f"  '(:ignore f :which-key "file")
-;  "ft" '(counsel-etags-list-tag-in-current-file :which-key "list tags in file")
-;  "fe" '(sr-speedbar-toggle :which-key "speedbar-toggle")
-;  "fo" '(ff-get-other-file  :which-key "switch header/source")
-;
-;  "g"  '(:ignore f :which-key "global")
-;  "gh" '(highlight-symbol-at-point :which-key "highlight point")
-;  "gu" '(unhighlight-regexp :which-key "unhighlight phrase")
-;
-;  "*" '(consult-ripgrep :which-key "search project")
-;
-;  "j"  '(:ignore f :which-key "jump")
-;  "jj" '(ace-jump-char-mode :which-key "Jump to Char")
-;  "jw" '(ace-jump-word-mode :which-key "Jump to Word")
-;  "jl" '(ace-jump-line-mode :which-key "Jump to Line")
-;
-;  "b"  '(:ignore f :which-key "buffer")
-;  "bb" '(consult-buffer :which-key "Change buffer")
-;)
-;;==================================================
+;; load breg lens
+;; (load-file "~/my_bigstore/alternate_home/emacs_configs/lsp-emacs/breglens.el")
 
 
 
+; (custom-set-variables
+;  ;; custom-set-variables was added by Custom.
+;  ;; If you edit it by hand, you could mess it up, so be careful.
+;  ;; Your init file should contain only one such instance.
+;  ;; If there is more than one, they won't work right.
+;  '(custom-safe-themes
+;    '("13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
+;  '(nil nil t)
+;  '(package-selected-packages
+;    '(evil-surround counsel-etags rg ripgrep sr-speedbar git-link dape esup general lsp-pyright lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode evil magit deadgrep doom-themes pyvenv python-black)))
+; (custom-set-faces
+;  ;; custom-set-faces was added by Custom.
+;  ;; If you edit it by hand, you could mess it up, so be careful.
+;  ;; Your init file should contain only one such instance.
+;  ;; If there is more than one, they won't work right.
+;  )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
- '(nil nil t)
- '(package-selected-packages
-   '(sr-speedbar git-link dape esup general lsp-pyright lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode evil magit deadgrep doom-themes pyvenv python-black)))
+   '("13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
+ '(package-selected-packages '(ripgrep yasnippet flycheck hydra)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
